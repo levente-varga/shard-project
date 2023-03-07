361f3561dd9d7d48f6b383a11c19f107ff3c698d
SimplemathicDev <v.levente1999@gmail.com>
2023-03-07 21:39:51 +0100

    Add Scene class

---
+++ Shard/ConsoleApp1/Shard/Scene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    class Scene
+    {
+        List<GameObject> gameObjects;
+
+        public Scene()
+        {
+            gameObjects = new List<GameObject>();
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
